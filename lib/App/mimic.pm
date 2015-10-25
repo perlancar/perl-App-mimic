90eedd405fa1a7a6cfb64181a2d5781483693980
perlancar (@pc-home) <perlancar@gmail.com>
2015-10-25 10:41:40 +0700

    Initial commit

---
+++ lib/App/mimic.pm
@@ -0,0 +1,16 @@
+package App::mimic;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Replace some characters with their Unicode homoglyphs
+
+=head1 DESCRIPTION
+
+This distribution contains the script L<mimic>.
+
+
+=head1 SEE ALSO
+
+L<mimic>
